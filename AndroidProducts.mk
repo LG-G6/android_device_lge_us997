9c551836c421d73aa836e39cdabc22a1ffe6fe60
Bernardo Bas <bernardobas@gmail.com>
2022-04-21 15:12:47 -0300

    us997: Evox-Ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_us997.mk
+    $(LOCAL_DIR)/evolution_us997.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_us997-userdebug \
-    lineage_us997-eng
+    evolution_us997-user \
+    evolution_us997-userdebug \
+    evolution_us997-eng
--- evolution_us997.mk
+++ evolution_us997.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from us997 device
 $(call inherit-product, device/lge/us997/device.mk)
@@ -30,7 +30,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH_OVERRIDE)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := us997
-PRODUCT_NAME := lineage_us997
+PRODUCT_NAME := evolution_us997
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-US997
 PRODUCT_MANUFACTURER := LGE
